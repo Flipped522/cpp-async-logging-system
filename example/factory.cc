d9c0e24c665af0204f923919f31f199d08a0f89b
Flipped522 <s9708240905@163.com>
2026-08-19 15:13:11 +0800

    工厂模式

--- example/factory.cc
+++ example/factory.cc
@@ -25,24 +25,50 @@ public:
     }
 };
 
+// class FruitFactory
+// {
+//     public:
+//     static std::shared_ptr<Fruit> create(const std::string &name){
+//         if(name == "苹果")
+//         {
+//             return std::make_shared<Apple>();
+//         }
+//         else
+//         {
+//             return std::make_shared<Banana>();
+//         }
+//     }
+// };
+
 class FruitFactory
 {
+public:
+    virtual std::shared_ptr<Fruit> create() = 0;
+};
+
+class AppleFacctory : public FruitFactory
+{
     public:
-    static std::shared_ptr<Fruit> create(const std::string &name){
-        if(name == "苹果")
-        {
-            return std::make_shared<Apple>();
-        }
-        else
-        {
-            return std::make_shared<Banana>();
-        }
+    std::shared_ptr<Fruit> create(){
+        return std::make_shared<Apple>();
+    }
+};
+
+class BananaFacctory : public FruitFactory
+{
+    public:
+    std::shared_ptr<Fruit> create(){
+        return std::make_shared<Banana>();
     }
 };
 
 int main()
 {
-    std::shared_ptr<Fruit> fruit = FruitFactory::create("苹果");
+    // std::shared_ptr<Fruit> fruit = FruitFactory::create("苹果");
+    // fruit->name();
+
+    std::shared_ptr<FruitFactory> ff(new AppleFacctory());
+    std::shared_ptr<Fruit> fruit = ff->create();
     fruit->name();
 
     return 0;
