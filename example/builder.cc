93eb2d508962cc54695f4398eb932c7e986da020
Flipped522 <s9708240905@163.com>
2026-08-20 16:36:00 +0800

    建造者模式

---
+++ example/builder.cc
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+class Computer
+{
+public:
+    Computer() {}
+
+    void setBoard(const std::string &board)
+    {
+        _board = board;
+    }
+
+    void setDisplay(const std::string &display)
+    {
+        _display = display;
+    }
+
+    virtual void setOs() = 0;
+
+    void showParameters()
+    {
+        std::string param = "Computer Parameters:\n";
+        param += "\tBoard: " + _board + "\n";
+        param += "\tDisplay: " + _display + "\n";
+        param += "\tOs: " + _os + "\n";
+        std::cout << param << std::endl;
+    }
+
+protected:
+    std::string _board;
+    std::string _display;
+    std::string _os;
+};
+
+class MacBook : public Computer
+{
+public:
+    void setOs() override
+    {
+        _os = "Mac OS x12";
+    }
+};
+
+class Builder
+{
+public:
+    virtual void buildBoard(const std::string &board) = 0;
+    virtual void buildDisplay(const std::string &display) = 0;
+    virtual void buildOs() = 0;
+    virtual std::shared_ptr<Computer> build() = 0;
+};
+
+class MacBookBuilder : public Builder
+{
+public:
+    MacBookBuilder() : _computer(new MacBook())
+    {
+    }
+
+    void buildBoard(const std::string &board) override
+    {
+        _computer->setBoard(board);
+    }
+
+    void buildDisplay(const std::string &display) override
+    {
+        _computer->setDisplay(display);
+    }
+
+    void buildOs() override
+    {
+        _computer->setOs();
+    }
+
+    std::shared_ptr<Computer> build() override
+    {
+        return _computer;
+    }
+
+private:
+    std::shared_ptr<Computer> _computer;
+};
+
+class Director
+{
+public:
+    Director(Builder *builder):_builder(builder)
+    {}
+
+    void construct(const std::string &board, const std::string& display)
+    {
+        _builder->buildBoard(board);
+        _builder->buildDisplay(display);
+        _builder->buildOs();
+    }
+
+private:
+    std::shared_ptr<Builder> _builder;
+};
+
+int main()
+{
+    Builder* builder = new MacBookBuilder();
+    std::unique_ptr<Director> director(new Director(builder));
+    director->construct("华硕", "三星显示器");
+    std::shared_ptr<Computer> computer = builder->build();
+    computer->showParameters();
+
+    return 0;
+}
